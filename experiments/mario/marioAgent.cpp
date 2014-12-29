86cf6ea7b227de414b1187bd5b504550216abe5a
Dave Kotfis <dkotfis@icloud.com>
2014-12-29 11:17:44 -0500

    Added missing mario agent source file.

---
+++ experiments/mario/marioAgent.cpp
@@ -0,0 +1,25 @@
+
+#include <vector>
+
+#include <rlglue/utils/C/RLStruct_util.h>
+
+int numActs = 12;
+int stateDimX = 20;
+int stateDimY = 12;
+
+action_t actionDecoder(const int act) {
+  action_t a;
+  return a;
+}
+
+int actionEncoder(const action_t act) {
+  int a;
+  return a;
+}
+
+std::vector<double> stateEncoder(const observation_t obs) {
+  std::vector<double> s;
+  return s;
+}
+
+
